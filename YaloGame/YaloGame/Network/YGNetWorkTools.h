e1dc0142ad2e9fb1656a810e206422d2aacc4a24
chengxu <chengxu@021.com>
2018-11-23 14:29:09 +0800

    add network.

---
+++ YaloGame/YaloGame/Network/YGNetWorkTools.h
@@ -0,0 +1,53 @@
+//
+//  YGNetWorkTools.h
+//  test
+//
+//  Created by C on 2018/11/22.
+//  Copyright © 2018 C. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "AFNetworking.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+
+typedef void(^SuccessBlock)(id responseObject);
+
+typedef void(^FailedBlock)(NSError *error);
+
+typedef void(^SessionBlock)(AFHTTPSessionManager *manager);
+
+@interface YGNetWorkTools : NSObject
+
+@property (nonatomic, assign) NSTimeInterval timeoutInterval;
+
+
+
++ (instancetype)sharedTools;
+
+- (void)get:(NSString *)url
+ parameters:(NSDictionary *)parameters
+    success:(SuccessBlock)success
+     failed:(FailedBlock)faild;
+
+- (void)post:(NSString *)url
+  parameters:(NSDictionary *)parameters
+     success:(SuccessBlock)success
+      failed:(FailedBlock)failed;
+
+- (void)get:(NSString *)url
+sessionConfig:(SessionBlock)block
+ parameters:(NSDictionary *)parameters
+    success:(SuccessBlock)success
+     failed:(FailedBlock)faild;
+
+- (void)post:(NSString *)url
+sessionConfig:(SessionBlock)block
+  parameters:(NSDictionary *)parameters
+     success:(SuccessBlock)success
+      failed:(FailedBlock)failed;
+
+@end
+
+NS_ASSUME_NONNULL_END
