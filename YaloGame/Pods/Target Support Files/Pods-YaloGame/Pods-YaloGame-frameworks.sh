27b8fc86c1f32203c9a53cfd5be0d20c52fb22db
chengxu <chengxu@021.com>
2018-12-07 16:15:26 +0800

    [M]update podfile.

--- YaloGame/Pods/Target Support Files/Pods-YaloGame/Pods-YaloGame-frameworks.sh
+++ YaloGame/Pods/Target Support Files/Pods-YaloGame/Pods-YaloGame-frameworks.sh
@@ -152,6 +152,7 @@ if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Masonry/Masonry.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SDCycleScrollView/SDCycleScrollView.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SDWebImage/SDWebImage.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/YYCache/YYCache.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/YYModel/YYModel.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
@@ -164,6 +165,7 @@ if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Masonry/Masonry.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SDCycleScrollView/SDCycleScrollView.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SDWebImage/SDWebImage.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/YYCache/YYCache.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/YYModel/YYModel.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
---
+++ YaloGame/YaloGame/Sections/Account/UserInfo/YGUserInfo.h
@@ -0,0 +1,17 @@
+//
+//  YGUserInfo.h
+//  YaloGame
+//
+//  Created by C on 2018/12/7.
+//  Copyright © 2018 C. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface YGUserInfo : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
